5f58609134ce8507eb3ade684d19c4e1ef28ff7f
lucthieulam <68269143+lucthieulam@users.noreply.github.com>
2022-03-19 11:38:22 +0700

    update

--- scss/js/form-material.js
+++ scss/js/form-material.js
@@ -2,7 +2,7 @@
     // Form material
     function inputFocusCheck(element) {
         //var $label = $(element).siblings('label');
-        if ($(element).val().length > 0) {
+        if ($(element).val() && $(element).val().length > 0) {
             $(element).addClass('has-value');
         } else {
             $(element).removeClass('has-value');
@@ -12,8 +12,11 @@
     $('.material-form .form-control').each(function () {
         inputFocusCheck(this);
     });
+    $('.material-form .form-select').each(function () {
+        inputFocusCheck(this);
+    });
     // The lines below (inside) are executed on change & keyup
-    $('.material-form .form-control').on('change keyup', function () {
+    $('.material-form .form-control, .material-form .form-select').on('change keyup', function () {
         inputFocusCheck(this);
     });
 })(jQuery);
